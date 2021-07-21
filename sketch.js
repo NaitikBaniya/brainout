8fde635280d9d1ac72f0c1b6adbc489aa7ac5d7b
sonalmehta1 <sonalmehta2701@gmail.com>
2021-07-21 21:02:23 +0530

    q1

--- sketch.js
+++ sketch.js
@@ -1,22 +1,32 @@
-var form1,form2, user,game;
-var gameState=0;
-var database;
-var userCount;
-var userIndex
-
-
+var gameState=1
+function preload(){
+    q1=loadImage("q1.jpg")
+    q2=loadImage("q2.jpg")
+    q3=loadImage("q3.jpg")
+}
 function setup() {
-    canvas = createCanvas(1000, 1000);
-    database=firebase.database();
-
-    user=new User();
-    user.getUserCount();
-   
-    game=new Game();
-    game.getState();
-    game.start();
+    canvas = createCanvas(displayWidth, displayHeight);
  
 }
 function draw() {
-    background(245);
+    background(200);
+    if(gameState===1){
+        var q1Text=createElement("h3");
+        q1Text.position(650,85);
+        q1Text.html("How many Ducks");
+
+        
+        imageMode(CENTER)
+        image(q1,width/2,height/3,width/4,height/2)
+
+        var A1=createRadio();
+        A1.style('font-size','30px');
+        A1.option("8");
+        A1.option("9");
+        A1.option("10");
+        A1.option("11");
+        A1.position(600,500);
+        
+    }
+    
 }
